03c59aae7f076afb3c6de720c6b5f0a501052d49
sd2108 <32153594+sd2108@users.noreply.github.com>
2017-09-26 18:18:33 +0530

    BookingDAO and its implementation completed

--- SeatBooking/src/main/java/com/dao/BookingDAO.java
+++ SeatBooking/src/main/java/com/dao/BookingDAO.java
@@ -1,5 +1,7 @@
-
+//Yet to be completed
 package com.seat.dao;
+
+import java.util.List;
 
 import org.hibernate.Session;
 
@@ -8,5 +10,7 @@ import com.seat.model.Booking;
 public interface BookingDAO {
 	
 	public void addBooking(Booking b,Session session);
-
+	public boolean swap(String employee_id1,String employee_id2,int seat1,int seat2);
+	public boolean cancelBooking(int seatno,Session session);
+	public List<Booking> viewBookings(Session session);
 }
